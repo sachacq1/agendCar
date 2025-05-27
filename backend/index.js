329ea80cac61580ae4a83e3d7afa9ada68b9b5c7
sachacq1 <sachacq1@gmail.com>
2025-05-27 16:26:40 -0300

    cambio cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors(
     {
-        origin: "https://agend-car.vercel.app/",
+        origin: "https://agend-car.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
