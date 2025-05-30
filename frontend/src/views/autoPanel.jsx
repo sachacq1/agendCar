b321613c591101081f1ea5275d595c065a5cff0b
sachacq1 <sachacq1@gmail.com>
2025-05-30 14:46:50 -0300

    cambios panel

--- frontend/src/views/autoPanel.jsx
+++ frontend/src/views/autoPanel.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { getAllCars, addCar, addMantenimiento } from "../services/apiCar.js";
+import { getAllCars, addCar } from "../services/apiCar.js";
+import { addMantenimiento } from "../services/apiMantenimiento.js";
 import axios from "../services/axiosInstance.js";
 
 const AutoPanel = () => {
