4edbc53be036dc1e6ac52d9ecd4529a4c311c12c
sachacq1 <sachacq1@gmail.com>
2025-06-01 16:00:44 -0300

    errores

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactDom from 'react-dom';
-import { AppRouter } from './routes/Routes.jsx';
+import AppRouter from './routes/Routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 
