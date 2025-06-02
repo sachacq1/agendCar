f671e8cca19bab0009b69881ff2f046cb57b88b5
sachacq1 <sachacq1@gmail.com>
2025-06-02 15:06:23 -0300

    context

--- frontend/src/views/autoPanel.jsx
+++ frontend/src/views/autoPanel.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext.jsx";
 
 const AutoPanel = () => {
     const { authToken } = useAuth();
